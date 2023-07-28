5dd2789e2938ce2c48bd1177c71acda53482f483
SarthakMoriya <sarthak8544@gmail.com>
2023-07-28 18:26:19 +0530

    navbar

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { HiArrowNarrowRight } from 'react-icons/hi'
+
+const Home = () => {
+    return (
+        <div className='h-screen w-full bg-[#0a192f]'></div>
+    )
+}
+
+export default Home
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import logo from '../assets/logo1.png'
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleNavbar = () => setNav(!nav)
+    return (
+        <div className='w-full h-[80px] fixed flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+            <div className="">
+                <img src={logo} alt="" className='w-[50px]' />
+            </div>
+            {/* Menu */}
+            <ul className='hidden md:flex'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Work</li>
+                <li>Contact</li>
+            </ul>
+            {/* Hamburger */}
+            <div className="md:hidden z-10" onClick={handleNavbar}>
+                {!nav ? <FaBars /> : <FaTimes />}
+            </div>
+            {/* Mobile Menu */}
+            {nav && (
+                <ul className=' absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center '>
+                    <li className='text-4xl py-6'>Home</li>
+                    <li className='text-4xl py-6'>About</li>
+                    <li className='text-4xl py-6'>Skills</li>
+                    <li className='text-4xl py-6'>Work</li>
+                    <li className='text-4xl py-6'>Contact</li>
+                </ul>
+            )}
+            {/* social Links */}
+            <div className="hidden lg:flex flex-col fixed top-[35%] left-0">
+                <ul>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500'>
+                        <a href="/" className='flex  justify-between items-center w-full text-gray-300 '>LinkedIn <FaLinkedin size={30} /></a>
+                    </li>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+                        <a href="/" className='flex  justify-between items-center w-full text-gray-300 '>Github <FaGithub size={30} /></a>
+                    </li>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+                        <a href="/" className='flex  justify-between items-center w-full text-gray-300 '>Email <HiOutlineMail size={30} /></a>
+                    </li>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+                        <a href="/" className='flex  justify-between items-center w-full text-gray-300 '>Resume <BsFillPersonLinesFill size={30} /></a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
